b4ea55fba4553ed0a2768ebca5efc37351390cac
ArcDatario <91425235+ArcDatario@users.noreply.github.com>
2025-08-18 02:27:40 +0800

    fix something

--- admin/includes/default-scripts.php
+++ admin/includes/default-scripts.php
@@ -1,6 +1,6 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/apexcharts@3.36.0/dist/apexcharts.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/simplebar@6.2.3/dist/simplebar.min.js"></script>
