39317067d0011ed218a8f509de785538e5557977
ArcDatario <91425235+ArcDatario@users.noreply.github.com>
2025-08-18 01:14:26 +0800

    jus fixing something

--- admin/backup/dashboard.php
+++ admin/backup/dashboard.php
@@ -8,11 +8,27 @@
   <!--  Body Wrapper -->
   <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
     data-sidebar-position="fixed" data-header-position="fixed">
-
     <!-- Sidebar Start -->
-    <?php include "includes/navbar.php";?>
+    <aside class="left-sidebar">
+      <!-- Sidebar scroll-->
+      <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+          <a href="./dashboard" class="text-nowrap logo-img">
+           <h4 class="logo-h4">Angeleyes</h4>
+          </a>
+          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+            <i class="ti ti-x fs-8"></i>
+          </div>
+        </div>
+
+        <!-- Sidebar navigation-->
+         <?php include "includes/navbar.php";?>
+        <!-- End Sidebar navigation -->
+         
+      </div>
+      <!-- End Sidebar scroll-->
+    </aside>
     <!--  Sidebar End -->
-      
     <!--  Main wrapper -->
     <div class="body-wrapper">
       <!--  Header Start -->
@@ -24,7 +40,12 @@
                 <i class="ti ti-menu-2"></i>
               </a>
             </li>
-            
+            <li class="nav-item">
+              <a class="nav-link nav-icon-hover" href="javascript:void(0)">
+                <i class="ti ti-bell-ringing"></i>
+                <div class="notification bg-primary rounded-circle"></div>
+              </a>
+            </li>
           </ul>
           <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
             <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -1,4 +1,17 @@
+<aside class="left-sidebar">
+      <!-- Sidebar scroll-->
+      <div>
+          <div class="brand-logo d-flex align-items-center justify-content-between">
+          <a href="./dashboard" class="text-nowrap logo-img">
+            <img src="../angeleyes-logo.png" alt="Angeleyes" class="logo-img" style="height:40px; object-fit:contain;">
+          </a>
+          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+            <i class="ti ti-x fs-8"></i>
+          </div>
+        </div>
 
+        <!-- Sidebar navigation-->
+       
         <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
           <ul id="sidebarnav">
             <li class="sidebar-item">
@@ -56,3 +69,8 @@
           </ul>
        
         </nav>
+        <!-- End Sidebar navigation -->
+         
+      </div>
+      <!-- End Sidebar scroll-->
+    </aside>
