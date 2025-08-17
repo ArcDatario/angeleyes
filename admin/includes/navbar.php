f10a683481ae7656e8e57fb4f7525c20d4944613
ArcDatario <91425235+ArcDatario@users.noreply.github.com>
2025-08-18 01:01:19 +0800

    just revise the nav bar

---
+++ admin/includes/navbar.php
@@ -0,0 +1,58 @@
+
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+          <ul id="sidebarnav">
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./dashboard" aria-expanded="false">
+                <span>
+                  <i class="ti ti-layout-dashboard"></i>
+                </span>
+                <span class="hide-menu">Dashboard</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./transactions.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-receipt"></i>
+                </span>
+                <span class="hide-menu">Transactions</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./plan.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-layout-grid"></i>
+                </span>
+                <span class="hide-menu">Plan</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./subscribers.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-users"></i>
+                </span>
+                <span class="hide-menu">Subscribers</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./admins.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-shield-check"></i>
+                </span>
+                <span class="hide-menu">Admins</span>
+              </a>
+            </li>
+            <li class="nav-small-cap">
+              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+              <span class="hide-menu">LOG OUT</span>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./logout.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-logout"></i>
+                </span>
+                <span class="hide-menu">Logout</span>
+              </a>
+            </li>
+          </ul>
+       
+        </nav>
